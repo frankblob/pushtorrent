ca68bc13531e9f0ca92da58f4f0544c9c879b67b
frank <frankblob@gmail.com>
2015-01-23 14:48:36 +0100

    Add list of torrent trackers to user page

--- app.rb
+++ app.rb
@@ -1,8 +1,9 @@
 require 'sinatra'
 require 'sequel'
 require 'sequel_secure_password'
-require 'nokogiri'
 require 'open-uri'
+require 'nokogiri'
+require 'active_support/core_ext/string/conversions'
 
 DB = Sequel.connect "sqlite://db/torwa-ar.db"
 Sequel::Model.plugin :timestamps, :update_on_create=>true
@@ -31,6 +32,7 @@ end
 post '/' do
 	redirect back if params[:keywords].empty?
 	keywords = params[:keywords].tr('^-a-zA-z0-9_.', ' ').split(' ').join('+')
+	#develop (1-4) fail-safe options, if torrentz.eu is down/blocking/non-responsive
 	url = 'https://torrentz.eu/feed?q=' + keywords
 	@keywords = keywords.split('+').join(' ')
 	data = Nokogiri::XML(open(url))
@@ -38,12 +40,49 @@ post '/' do
 	@items.empty? ? (erb :zerohits) : (erb :results)
 end
 
-get '/user' do
+get '/login' do # display login form
 	erb :login
 end
 
-post '/user' do
-	erb :userpage
+post '/login' do
+	#create user session
+	redirect_back
+end
+
+get '/signup' do # display signup form
+	@user = User.new
+	erb :signup #redirect signup-confirmation, back, home or user?
+end
+
+post '/signup' do #create user
+	erb :signup_confirm #|| redirect :back, :home or :user?
+end
+
+get '/user/:id' do
+	#if user == logged in
+	usertrackers = UserTracker.where(user_id: (params[:id])).all
+	@trackers = usertrackers.map{|x| x.tracker}
+	erb :user
+	#if user != logged in
+	#erb :login
+	#erb :loginorsignup / :access
+end
+
+post '/tracker' do
+#if current_user does not exists:
+
+#if email does not exist
+# display email + torrent notification and settings
+
+#if email does exist, with user_type = 0
+#display current tracker, offer to replace and to signup
+
+#if email does exist, with user_type = 1 || 2
+#display login page
+
+#if current_user exists:
+#add and display added + highlighted tracker in user's tracker list
+
 end
 
 get '/about' do
@@ -52,10 +91,6 @@ end
 
 get '/contact' do
 	erb :contact
-end
-
-get '/signup' do
-	erb :signup
 end
 
 get '/forum' do
