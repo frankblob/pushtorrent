b49b297ecaa689c634142eb8f015e19687dae46f
frank <frankblob@gmail.com>
2015-03-15 13:23:51 +0100

    Implement setting user name via profile page.

--- controllers/user.rb
+++ controllers/user.rb
@@ -9,3 +9,15 @@ get '/user/?' do
 	end
 
 end
+
+patch '/user/?' do
+
+	if user = User.get(session[:user_id])
+		user.name = params[:user][:name]
+		user.save
+		flash[:success] = "You have added your name successfully, #{user.name}. Enjoy!"
+		redirect '/user'
+	else
+		redirect '/login'
+	end
+end
